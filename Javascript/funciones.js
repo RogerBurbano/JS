08c6698c6838c3cf296f13a8001b90f4c9b9f94c
Roger burbano <coyote2803@gmail.com>
2022-02-17 12:04:04 -0500

    JS

--- Javascript/funciones.js
+++ Javascript/funciones.js
@@ -1,11 +1,32 @@
 
 
-function producto(){
+// un parametro con return************************************************
+function saludar(nombreRey){
 
-    let prod = prompt('Seleccione que producto desea llevar')
-
+     return ('hola ' + nombreRey) //(`Hola señor ${nombreRey}`)
+     
 
 
 }
+  console.log(saludar('Roger')); 
 
-producto()
+  // dos parametros con return*************************************************
+
+  
+  
+  
+  function suma (n1, n2){
+        return parseInt(n1) + parseInt(n2)
+  }
+  console.log(suma(7, 7))
+
+
+//Suma dinamica*********************************************************************************
+
+  function sumar (n1, n2){
+    return parseInt(n1) + parseInt(n2)
+  }
+ let numUno = prompt('Ingrese un numero')
+ let numDos = prompt('Ingrese un nuevo numero')
+
+ console.log(sumar(numDos, numUno)) // no importa el nombre , es el parametro 
